840c75ce4371c290c378ead615e0c9dd122e412d
Stephen Dawson-Haggerty <sdhags@gmail.com>
2024-09-25 10:07:31 -0400

    Finish implementation

--- addone.js
+++ addone.js
@@ -5,6 +5,9 @@ const NormalSdk = require("@normalframework/applications-sdk");
  * @param {NormalSdk.InvokeParams} params
  * @returns {NormalSdk.InvokeResult}
  */
-module.exports = async ({points, sdk, update, args}) => {
-  
+    module.exports = async ({points, sdk, update, args}) => {
+        console.log(points.length)  
+        for (let i = 0; i < points.length; i++) {
+            await points[i].write(points[i].latestValue.value + 1)
+        }
 };
